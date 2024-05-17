79e979ef5d0cec931847e485a27c41e36f372e04
JeenathBandi <167793445+JeenathBandi@users.noreply.github.com>
2024-05-17 09:17:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -85,6 +85,7 @@ app.get("/players/:playerId/matches", async (request, response) => {
     SELECT match_details.match_id,match_details.match,match_details.year
     FROM 
         match_details JOIN player_match_score
+        ON match_details.match_id = player_match_score.match_id
     WHERE 
         player_match_score.player_id = ${playerId};`;
   const dbResponse = await db.all(getMatchIdQuery);
@@ -94,7 +95,9 @@ app.get("/players/:playerId/matches", async (request, response) => {
 app.get("/matches/:matchId/players", async (request, response) => {
   const { matchId } = request.params;
   const getPlayersQuery = `
-        SELECT player_details.player_id,player_details.player_name FROM player_details JOIN player_match_score
+        SELECT player_details.player_id,player_details.player_name 
+        FROM player_details JOIN player_match_score
+         ON player_details.player_id = player_match_score.player_id
         WHERE player_match_score.match_id  = ${matchId};
     `;
   const dbArray = await db.all(getPlayersQuery);
